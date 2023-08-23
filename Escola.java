87ab8b1493201cdb4da538e00423f48d90c378de
Lukas Wendel <lukas.wrks@gmail.com>
2023-08-23 19:56:54 -0300

    Update Escola.java

--- Escola.java
+++ Escola.java
@@ -1,4 +1,3 @@
-
 public class Escola {
 
 	public static void main(String[] args) {
@@ -11,19 +10,66 @@ public class Escola {
 		Curso licenciaturaQuimica = new Curso();
 		Curso processosQuimicos = new Curso();
 		
-		Departamento dpt = new Departamento();
+		Departamento daic = new Departamento();
+		Departamento dqa = new Departamento();
+		Departamento dqi = new Departamento();
+		Departamento dainfra = new Departamento();
+		
+		Campus cmc = new Campus();
+		Campus cmdi = new Campus();
+		Campus cmzl = new Campus();
+		
+		// Campus
+		
+		cmc.sigla = "CMC";
+		cmc.nome = "Campus Manaus Centro";
+		cmc.cidade = "Manaus";
+		cmc.bairro = "Centro";
+		
+		cmdi.sigla = "CMDI";
+		cmdi.nome = "Campus Manaus Distrito Industrial";
+		cmdi.cidade = "Manaus";
+		cmdi.bairro = "Distrito Industrial";
+		
+		cmzl.sigla = "CMZL";
+		cmzl.nome = "Campus Manaus Zona Leste";
+		cmzl.cidade = "Manaus";
+		cmzl.bairro = "São José Operário";
+		
+		// Departamentos
+		
+		daic.sigla = "DAIC";
+		daic.nome = "Departamento Acadêmico de Informação e Comunicação";
+		daic.campus = cmc;
+				
+		dqa.sigla = "DQA";
+		dqa.nome = "Departamento Acadêmico de Química e Alimentos";
+		dqa.campus = cmc;
+				
+		dqi.sigla = "DQI";
+		dqi.nome = "Departamento Acadêmico de Produção Industrial";
+		dqi.campus = cmdi;
+				
+		dainfra.sigla = "DAINFRA";
+		dainfra.nome = "Departamento Acadêmico de Infraestrutura";
+		dainfra.campus = cmzl;
+				
+		
 		// Cursos
 		tads.sigla = "TADS";
 		tads.nome = "Tecnologia em Análise e Desenvolvimento de Sistemas";
 		tads.duracao = 6;
+		tads.departamento = daic;
 		
 		licenciaturaQuimica.sigla = "LQ";
 		licenciaturaQuimica.nome = "Licenciatura em Química";
 		licenciaturaQuimica.duracao = 8;
+		licenciaturaQuimica.departamento = dqa;
 		
 		processosQuimicos.sigla = "PQ";
 		processosQuimicos.nome = "Processos Qúmicos";
 		processosQuimicos.duracao = 8;
+		processosQuimicos.departamento = dqa;
 		
 		// Alunos
 		
@@ -38,7 +84,7 @@ public class Escola {
 		wallace.nome = "Wallace";
 		wallace.matricula = "2023005621";
 		wallace.curso = processosQuimicos;
-		
+
 		a = isadora; // Não é uma cópia, 'a' recebe a própria instância de isadora;
 		//a.curso = "Direito"; // Altera o valor do atributo de isadora
 		
@@ -49,7 +95,11 @@ public class Escola {
 	}
 	
 	public static void mostrarAluno(Aluno aluno) {
-		System.out.println("Aluno: " + aluno.nome + ", matricula: " + aluno.matricula + ", curso: " + aluno.curso.nome);
+		System.out.println("Aluno: " + aluno.nome 
+							+ ", matricula: " + aluno.matricula 
+							+ ", curso: " + aluno.curso.nome 
+							+ ", do departamento: " + aluno.curso.departamento.sigla
+							+ ", do campus: " + aluno.curso.departamento.campus.sigla);
 	}
 	
 }
